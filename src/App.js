0ee00a7165904de39791f9cd6032c71be51e1f7d
hasharas <hasharamsankalpam@gmail.com>
2024-02-17 13:28:43 +0530

    build basic things

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// 6c8b9677
+
+const API_URL = 'http://www.omdbapi.com?apikey=6c8b9677';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+
+        searchMovies('Spiderman');
+
+    }, []);
+
     return(
         <h1>App</h1>
     );
